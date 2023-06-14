6e6356d92bb9eed8d7bee4fe5ebdb9c8e409d422
Reef Ashkenazi <102466625+rif281@users.noreply.github.com>
2023-06-14 15:00:11 +0300

    Update accounts_page.py

--- pages/accounts_page.py
+++ pages/accounts_page.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-test = ""
 
 class AccountsPage():
 
